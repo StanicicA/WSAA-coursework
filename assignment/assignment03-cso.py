6ecca380a2d29df07acd2f06d1b9fd9b176ac57e
StanicicA <g00411439@gmit.ie>
2024-04-28 20:49:39 +0100

    assignment03

---
+++ assignment/assignment03-cso.py
@@ -0,0 +1,18 @@
+from ast import Pass
+import requests
+import json
+
+# the link to the url = "https://ws.cso.ie/public/api.restful/PxStat.Data.Cube_API.ReadDataset/FIQ02/JSON-stat/2.0/en"
+
+urlBegining = "https://ws.cso.ie/public/api.restful/PxStat.Data.Cube_API.ReadDataset/"
+urlEnd = "/JSON-stat/2.0/en"
+
+def getAll(dataset):
+    url = urlBegining + dataset + urlEnd
+    response = requests.get(url)
+    return response.json()
+
+if __name__ == "__main__":
+    with open("cso.json","wt") as fp:
+         print(json.dumps(getAll("FIQ02")),file=fp)
+
